4daf72b9dd2bba823e6acde9f59cbb43fa1d9d66
nadh06 <2023435818@student.uitm.edu.my>
2025-01-02 21:46:55 +0800

    "Adding Google Authentication"

---
+++ src/firebase-config.js
@@ -0,0 +1,11 @@
+export const environment = {
+  firebase: {
+    apiKey: "AIzaSyDMSP2PONMNv4iyRsBgsIF7qf8sBYpblCc",
+    authDomain: "friendlychat-5a09e.firebaseapp.com",
+    projectId: "friendlychat-5a09e",
+    storageBucket: "friendlychat-5a09e.firebasestorage.app",
+    messagingSenderId: "36603489317",
+    appId: "1:36603489317:web:a46426546eca70e06863cc",
+    measurementId: 'G-MEASUREMENT_ID',
+  },
+};
